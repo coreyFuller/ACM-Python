17b4f2fa252ff4cd833e64b0ec18a0e6517afafe
unknown <coreyfuller00@gmail.com>
2020-04-14 01:23:09 -0400

    Added Example Code

---
+++ Session3/import_mod.py
@@ -0,0 +1,11 @@
+import my_module as mm
+
+
+def main():
+    mm.printGreeting()
+    mm.printResponse()
+    
+    
+if __name__ == '__main__':
+    main()
+    
---
+++ Session3/my_module.py
@@ -0,0 +1,7 @@
+def printGreeting():
+    print("Hello there.")
+    return True
+
+def printResponse():
+    print("General Kenobi. You are a bold one.")
+    return False
